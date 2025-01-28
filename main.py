8098897c403610be93d984f892d7e72cc6500c55
yar kyrylenko <ya.kyrylenko@gmail.com>
2025-01-28 21:29:38 +0200

    Add updatedAt and createdAt atributes to task dict Add IndexError handling when theare less atributes than expected

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import json
 import os
 import sys
+from datetime import datetime
 
 TASK_FILE_NAME = 'tasks.json'
 
@@ -20,22 +21,32 @@ def read_tasks(file_name):
 
 def add_new_task(description, status = 'todo'):
     tasks = read_tasks(TASK_FILE_NAME)
+    date_time = get_time()
 
     new_task = {
         'id': len(tasks) + 1,
         'desc': description,
         'status': status,
+        'createdAt': date_time,
+        'updatedAt': date_time
     }
     tasks.append(new_task)
     save_to_file(tasks)
     print(f"Task added successfully (ID: {new_task['id']})")
 
+def get_time():
+    return datetime.now().isoformat()
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     args = sys.argv[1:]
     if args[0] == 'add':
-        print("Adding new task")
-        add_new_task(args[1])
+        try:
+            add_new_task(args[1])
+        except IndexError:
+            print("After 'add' command you should specify task message")
+            print("Example: add 'Visit grandma'")
+            print("Notice you should add quotes at start and end of the message")
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
