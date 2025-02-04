be967c1333dd38773678487bf40d29df101f6af1
yar kyrylenko <ya.kyrylenko@gmail.com>
2025-02-04 16:46:38 +0200

    Add messages if script runs without any args

--- main.py
+++ main.py
@@ -94,50 +94,54 @@ def clear_tasks():
 if __name__ == '__main__':
     args = sys.argv[1:]
     tasks = read_tasks(TASK_FILE_NAME)
-    if args[0] == 'add':
-        try:
-            task_message = args[1]
-            add_new_task(tasks, task_message)
-        except IndexError:
-            print("After 'add' command you should specify task message")
-            print("Example: add 'Visit grandma'")
-            print("Notice you should add quotes at start and end of the message")
-    elif args[0] == 'list':
-        if len(args) > 1:
-            status = args[1]
-            list_tasks(tasks, status)
-        else:
-            list_tasks(tasks)
-    elif args[0] == 'update':
-        try:
-            index = int(args[1])
-            description = args[2]
-            update_task(tasks, index, description)
-        except ValueError:
-            print("Error: ID must be a number.")
-        except IndexError:
-            print("After 'update' command you should specify id for desired task and updated task message")
-            print("Example: update 1 'Visit grandma and grandfather'")
-    elif args[0] == 'delete':
-        try:
-            index = int(args[1])
-            delete_task(tasks, index)
-        except ValueError:
-            _number_notice()
-        except IndexError:
-            print("After 'delete' command you should specify id for desired task")
-            print("Example: delete 1")
-    elif args[0] in ['mark-in-progress', 'mark-done']:
-        try:
-            _, _, status = args[0].partition('-')
-            id = int(args[1])
-            change_status(tasks, status, id)
-        except IndexError:
-            print("After 'mark-in-progress' or 'mark-done' commands you should specify desired id")
-            print('Example: mark-done 2')
-        except ValueError:
-            _number_notice()
-    elif args[0] == 'clear':
-        clear_tasks()
+    if len(args) == 0:
+        print("Please add some info, example: add 'Task description'")
+        print("You also can update, delete, list tasks")
+    elif len(args) > 1:
+        if args[0] == 'add':
+            try:
+                task_message = args[1]
+                add_new_task(tasks, task_message)
+            except IndexError:
+                print("After 'add' command you should specify task message")
+                print("Example: add 'Visit grandma'")
+                print("Notice you should add quotes at start and end of the message")
+        elif args[0] == 'list':
+            if len(args) > 1:
+                status = args[1]
+                list_tasks(tasks, status)
+            else:
+                list_tasks(tasks)
+        elif args[0] == 'update':
+            try:
+                index = int(args[1])
+                description = args[2]
+                update_task(tasks, index, description)
+            except ValueError:
+                print("Error: ID must be a number.")
+            except IndexError:
+                print("After 'update' command you should specify id for desired task and updated task message")
+                print("Example: update 1 'Visit grandma and grandfather'")
+        elif args[0] == 'delete':
+            try:
+                index = int(args[1])
+                delete_task(tasks, index)
+            except ValueError:
+                _number_notice()
+            except IndexError:
+                print("After 'delete' command you should specify id for desired task")
+                print("Example: delete 1")
+        elif args[0] in ['mark-in-progress', 'mark-done']:
+            try:
+                _, _, status = args[0].partition('-')
+                id = int(args[1])
+                change_status(tasks, status, id)
+            except IndexError:
+                print("After 'mark-in-progress' or 'mark-done' commands you should specify desired id")
+                print('Example: mark-done 2')
+            except ValueError:
+                _number_notice()
+        elif args[0] == 'clear':
+            clear_tasks()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
